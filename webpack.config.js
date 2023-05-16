fa3f3034b6747e0cb04741c8e201e08f6c8a96e7
stevecyj <ycsteven.tsao@gmail.com>
2023-05-16 12:03:51 +0800

    build: configure webpack for development environment
    - Create a new file `webpack.config.js`
    - Add an entry point to `./src/index.js`
    - Set the output path to `dist` and filename to `main.js`
    - Add an empty rule array to the module section
    - Add an empty plugin array to the plugins section
    - Set the mode to `development`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports  = {
+//   入口
+entry: './src/index.js',
+//   輸出
+output: {
+  path: path.resolve(__dirname, 'dist'),
+  filename: 'main.js'
+},
+//   loader
+module: {
+  rules: [
+//     loader config
+  ],
+},
+//   plugins
+plugins: [
+//   plugins config
+],
+//   mode
+mode: 'development'
+}
