67a9fd9009e5f28ccfe3aebd9898dcb20819fcb0
stevecyj <ycsteven.tsao@gmail.com>
2023-05-22 21:36:33 +0800

    build: update build configuration for production deployment
    - Change the script source path to ../dist/main.js in public/index.html
    - Comment out the import of css file in src/main.js
    - Uncomment the mode: 'production' line in webpack.config.js
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import count from './js/count';
 import sum from './js/sum';
-import './css/index.css';
+// import './css/index.css';
 
 console.log('count(2, 1) = ', count(2, 1));
-console.log('sum(1, 2,3,4) = ', sum(1, 2,3,4));
+console.log('sum(1, 2,3,4) = ', sum(1, 2, 3, 4));
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,24 @@
 const path = require('path');
 
-module.exports  = {
-//   入口
-entry: './src/main.js',
-//   輸出
-output: {
-  path: path.resolve(__dirname, 'dist'),
-  filename: 'main.js'
-},
-//   loader
-module: {
-  rules: [
-//     loader config
+module.exports = {
+  //   入口
+  // entry: './src/main.js',
+  //   輸出
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  //   loader
+  module: {
+    rules: [
+      //     loader config
+    ],
+  },
+  //   plugins
+  plugins: [
+    //   plugins config
   ],
-},
-//   plugins
-plugins: [
-//   plugins config
-],
-//   mode
-mode: 'development'
-}
+  //   mode
+  mode: 'development',
+  // mode: 'production',
+};
